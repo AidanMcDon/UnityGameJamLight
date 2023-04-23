31e22447c3bafc322dfa6ddeac1b7351809d112d
Ryan-Claus <ryanparkerclaus@gmail.com>
2023-04-23 00:13:44 -0400

    Planet moves offscreen when killed

--- Light/Assets/Scripts/PlanetBehavior.cs
+++ Light/Assets/Scripts/PlanetBehavior.cs
@@ -6,23 +6,28 @@ using UnityEngine;
 public class PlanetBehavior : MonoBehaviour
 {
     public bool PlanetDead = false;
-    public float PlanetHealth;
+    public float PlanetHealth = 50;
     public GameObject Planet;
-    private float CurrentHealth;
+    private float CurrentHealth, StartingHealth;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        //private float NewPlanetHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
-       if(PlanetDead == true)
+       if(PlanetDead == true) //check if planet is dead, move offscreen, give more health
         {
+            Planet.transform.position = new Vector3(20, 20, 0); //teleports planet off screen
+            //NewPlanetHealth = PlanetHealth + 20;
+            //PlanetHealth = NewPlanetHealth;
+            //Destroy(Planet);
 
-            Destroy(Planet);
+                        //change art to next planet
+            PlanetDead = false;
         }
     }
 
